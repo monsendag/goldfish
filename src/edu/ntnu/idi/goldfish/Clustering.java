dc6675b2fc9327dbd20512313cc9bc936697a962
Patrick Romstad <promstad1@gmail.com>
2013-11-04 17:16:58 +0100

    Clustering soon to be finished

--- src/edu/ntnu/idi/goldfish/Clustering.java
+++ src/edu/ntnu/idi/goldfish/Clustering.java
@@ -1,33 +1,27 @@
 package edu.ntnu.idi.goldfish;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
-import org.antlr.grammar.v3.ANTLRv3Parser.finallyClause_return;
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
-import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.clustering.classify.WeightedVectorWritable;
 import org.apache.mahout.clustering.kmeans.KMeansDriver;
 import org.apache.mahout.clustering.kmeans.Kluster;
@@ -35,11 +29,14 @@ import org.apache.mahout.common.HadoopUtil;
 import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
 import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.NamedVector;
-import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.VectorWritable;
-import org.apache.xerces.xs.ItemPSVI;
 
 public class Clustering {
+	
+	private static Map<Integer, Integer> userPositions;
+	private static Map<Integer, Integer> itemPositions;
+	private static List<RatingPair> ratingPairs;
+	private static Map<Integer, Integer> userToItemMap;
 	
 	public static void writeUsersToFile(List<NamedVector > users, Path path, Configuration conf) throws IOException {
 		FileSystem fs = FileSystem.get(path.toUri(), conf);
@@ -60,18 +57,25 @@ public class Clustering {
 		
 		double[][] userItemMatrix = getMatrix("datasets/vtt-36k/VTT_I_data.csv");
 		NamedVector user;
-		for (int i = 0; i < userItemMatrix.length; i++) {
-			user = new NamedVector(new DenseVector(userItemMatrix[i]), "User " + i);
+		
+		Set<Integer> userIds = userPositions.keySet();
+
+		int iterator = 0;
+		for (Integer userId : userIds) {
+			user = new NamedVector(new DenseVector(userItemMatrix[iterator]), ""+userId);
 			users.add(user);
+			iterator++;
 		}
 		
 		return users;
 	}
 	
 	public static double[][] getMatrix(String csvFile) throws FileNotFoundException {
-		Map<Integer, Integer> itemPosistions = new HashMap<Integer, Integer>();
-		Map<Integer, Integer> userPositions = new HashMap<Integer, Integer>();
-		List<RatingPair> ratingPairs = new ArrayList<RatingPair>();
+		itemPositions = new HashMap<Integer, Integer>();
+		userPositions = new HashMap<Integer, Integer>();
+		ratingPairs = new ArrayList<RatingPair>();
+		userToItemMap = new HashMap<Integer, Integer>();
+		
 		
 		int itemPosition = 0;
 		int userPosition = 0;
@@ -83,37 +87,38 @@ public class Clustering {
 		int counter = 0;
 		
 		RatingPair pair;
-		while(sc.hasNextLine() && counter++ <= 10000) {
+		while(sc.hasNextLine() && counter++ <= 100) {
 			
 			pair = new RatingPair(sc.nextInt(), sc.nextInt());
 			sc.nextInt(); // skip rating
 			
 			ratingPairs.add(pair);
+			userToItemMap.put(pair.userId, pair.itemId);
 			
 			if(!userPositions.containsKey(pair.userId)) {
 				userPositions.put(pair.userId, userPosition);
 				userPosition++;
 			}
 			
-			if(!itemPosistions.containsKey(pair.itemId)) {
-				itemPosistions.put(pair.itemId, itemPosition);
+			if(!itemPositions.containsKey(pair.itemId)) {
+				itemPositions.put(pair.itemId, itemPosition);
 				itemPosition++;
 			}
 			
 		}
 		sc.close();
 		
-		double[][] userItemMatrix = new double[userPosition + 1][itemPosition + 1];
+		double[][] userItemMatrix = new double[userPosition][itemPosition];
 		
 		for (RatingPair ratingPair : ratingPairs) {
-			userItemMatrix[userPositions.get(ratingPair.userId)][itemPosistions.get(ratingPair.itemId)] = 1;
+			userItemMatrix[userPositions.get(ratingPair.userId)][itemPositions.get(ratingPair.itemId)] = 1;
 		}
 
 		return userItemMatrix;
 	}
 
 	public static void main(String[] args) throws TasteException, IOException, InterruptedException, ClassNotFoundException {
-		int numberOfCluster = 2;
+		int numberOfCluster = 3;
 		
 		List<NamedVector> users = getNamedVector();
 		
@@ -145,22 +150,86 @@ public class Clustering {
 		HadoopUtil.delete(conf, output);
 		
 		// Run k-means algorithm
-		KMeansDriver.run(conf, new Path("testdata/points"), new Path("testdata/clusters"), output, 
-				new EuclideanDistanceMeasure(), 0.001, 10, true, 0.0, false);
+		KMeansDriver.run(
+				conf, 							// configuration
+				new Path("testdata/points"), 	// the directory pathname for input points
+				new Path("testdata/clusters"),  // the directory pathname for initial & computed clusters
+				output, 						// the directory pathname for output points
+				new EuclideanDistanceMeasure(), // the DistanceMeasure to use
+				0.001, 							// the convergence delta value
+				10, 							// the maximum number of iterations
+				true, 							// true if points are to be clustered after iterations are completed
+				0.0, 							// clustering strictness/ outlier removal parameter. 
+				false							// if true execute sequental algorithm
+			);
 		
 		SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path("output/" 
 				+ Kluster.CLUSTERED_POINTS_DIR + "/part-m-00000"), conf);
 		
-		IntWritable key = new IntWritable();
+		IntWritable clusterId = new IntWritable();
 		WeightedVectorWritable value = new WeightedVectorWritable();
-		while (reader.next(key, value)) {
+		Map<String, String> clusters = new HashMap<String, String>(); 
+		while (reader.next(clusterId, value)) {
 			// Read output, print vector, cluster ID 
 			NamedVector vector = (NamedVector) value.getVector();
 			String vectorName = vector.getName();
-			System.out.println(vectorName + " belongs to cluster " + key.toString());
+			System.out.println(vectorName + " belongs to cluster " + clusterId.toString());
+			clusters.put(vectorName, clusterId.toString());
 		}
 		reader.close();
+		
+		writeClustersToFile(clusters, numberOfCluster);
 	
+	}
+	
+	public static void writeClustersToFile(Map<String, String> clusters, int numberOfClusters) throws IOException {
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		String fileName;
+		
+		File testData = new File("datasets/vtt-clustered");
+		if (!testData.exists()) {
+			testData.mkdir();
+		}
+		
+		for (File file : testData.listFiles()) {
+			file.delete();
+		}
+		
+		
+		
+		// for each cluster
+		
+			// for each user, 
+		
+				// for each rating in userItemMatrix[user][i]
+		
+					// if rating != 0
+						//	get item id from inverted itemPositions
+						// write line: userid,itemid,1
+		
+		for (int i = 0; i < numberOfClusters; i++) {
+			fileName = "datasets/vtt-clustered/cluster" + i + ".csv";
+			fw = new FileWriter(fileName);
+			pw = new PrintWriter(fw);
+			
+			for (Entry<String, String> entry : clusters.entrySet()) {
+				String clusterId = entry.getValue();
+				String userId = entry.getKey();
+				if(i == Integer.parseInt(clusterId)) {
+					pw.print(userId);
+					pw.print(",");
+					pw.print(userToItemMap.get(Integer.parseInt(userId)));
+					pw.print(",");
+					pw.println(1);
+				}
+			}
+			
+			pw.flush();
+		}
+		
+		pw.close();
+		fw.close();
 	}
 	
 
