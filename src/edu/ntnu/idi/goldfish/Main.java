bbda5ad9fa53c0d1457c3b822431b8e9a627adc3
Dag Einar Monsen <me@dag.im>
2013-11-06 13:33:07 +0100

    Various changes to Main.

--- src/edu/ntnu/idi/goldfish/Main.java
+++ src/edu/ntnu/idi/goldfish/Main.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
+import org.apache.lucene.benchmark.quality.QualityStats.RecallPoint;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.example.grouplens.GroupLensDataModel;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
 
+import edu.ntnu.idi.goldfish.EvaluationResult.SortOption;
 import edu.ntnu.idi.goldfish.MemoryBased.Similarity;
 
 
@@ -18,28 +22,21 @@ public class Main {
 	// disable Mahout logging output
 	static { System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog"); }
 	
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws TasteException 
+		
+	/*
+	 * MEMORY-based evaluation 
+	 * 
+	 * Evaluate KNN and Threshold based neighborhood models.
+	 * Try all different similarity metrics found in ModelBased.Similarity.
 	 */
-	public static void main(String[] args) throws IOException, TasteException {
+	public static List<EvaluationResult> evaluateMemoryBased(DataModel dataModel) throws IOException, TasteException {
+		Evaluator evaluator = new Evaluator();
 		
-		//DataModel model = new GroupLensDataModel(new File("data/movielens-1m/ratings.dat.gz"));
-		DataModel dataModel = new GroupLensDataModel(new File("datasets/sample100/ratings.dat.gz"));
 		
-		Evaluator evaluator = new Evaluator(dataModel);
-		
-		/*
-		 * MEMORY-based evaluation 
-		 * 
-		 * Evaluate KNN and Threshold based neighborhood models.
-		 * Try all different similarity metrics found in ModelBased.Similarity.
-		 */
-		for(Similarity similarity : Similarity.values()) {
+			for(Similarity similarity : Similarity.values()) {
 			
-			// KNN: try different neighborhood sizes
-			for(int K = 7; K >= 1; K -= 1) {
+			// KNN: try different neighborhood sizes (odd numbers are preferable)
+			for(int K = 15; K >= 1; K -= 2) {
 				evaluator.add(new KNN(similarity, K));			
 			}
 			
@@ -49,22 +46,51 @@ public class Main {
 			}
 		}
 
-		ArrayList<EvaluationResult> results = evaluator.evaluateAll();
+		 return evaluator.evaluateAll(dataModel, 0.90, 0.10, 10);
+	}
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws TasteException 
+	 * @throws ClassNotFoundException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws IOException, TasteException, InterruptedException, ClassNotFoundException {
+		
+		//DataModel model = new GroupLensDataModel(new File("data/movielens-1m/ratings.dat.gz"));
+		DataModel dataModel = new GroupLensDataModel(new File("datasets/sample100/ratings.dat.gz"));
+		//DataModel dataModel = new FileDataModel(new File("datasets/vtt-clustered/cluster0.csv"));
+		
+		List<EvaluationResult> results = evaluateMemoryBased(dataModel);
 		
 		
-		for(EvaluationResult res : results) {
-			System.out.println(res);
-		}
-
+		
 		/**
 		 * MODEL-based evaluation
 		 */
 		
 		// clustering models (KMeans ... EM?)
 		
+		DataModel[] dataModels = UserClusterer.clusterUsers(dataModel, 5);
+		List<EvaluationResult> res;
+		for(DataModel clusteredModel : dataModels) {
+			res = evaluateMemoryBased(clusteredModel);
+		}
+		
 		// latent semantic models (Matrix Factorizations, etc..)
 		
 		
+		EvaluationResult.sortList(results, SortOption.RMSE);
 		
+		for(EvaluationResult re : results) {
+			System.out.println(re);
+		}
 	}
+	
+
+	
+	
+	
+	
 }
