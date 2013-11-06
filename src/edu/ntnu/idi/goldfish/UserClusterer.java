85cf897c0741472e91095133d269da7fb0cf93a3
Dag Einar Monsen <me@dag.im>
2013-11-06 17:21:48 +0100

    Change UserClusterer to work with DataModel types instead of flat files.
    It currently crashes because of memory consumption. Need to look into this.

--- src/edu/ntnu/idi/goldfish/UserClusterer.java
+++ src/edu/ntnu/idi/goldfish/UserClusterer.java
@@ -1,185 +1,65 @@
 package edu.ntnu.idi.goldfish;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Scanner;
-import java.util.Set;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.SequenceFile.Writer;
 import org.apache.hadoop.io.Text;
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.FastByIDMap;
+import org.apache.mahout.cf.taste.impl.model.GenericDataModel;
+import org.apache.mahout.cf.taste.impl.model.GenericItemPreferenceArray;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.clustering.classify.WeightedVectorWritable;
+import org.apache.mahout.cf.taste.model.PreferenceArray;
 import org.apache.mahout.clustering.kmeans.KMeansDriver;
 import org.apache.mahout.clustering.kmeans.Kluster;
 import org.apache.mahout.common.HadoopUtil;
-import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
-import org.apache.mahout.common.distance.TanimotoDistanceMeasure;
+import org.apache.mahout.common.distance.DistanceMeasure;
 import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.NamedVector;
 import org.apache.mahout.math.VectorWritable;
 
 public class UserClusterer {
-	
-	private static Map<Integer, Integer> userPositions;
-	private static Map<Integer, Integer> itemPositions;
-	private static List<RatingPair> ratingPairs;
-	private static double[][] userItemMatrix;
 
 	/**
 	 * Split the dataModel into a set of datamodels based on a kMeans clustering of the users
-	 * @param dataModel
-	 * @param N
-	 * @return
-	 * @throws ClassNotFoundException 
-	 * @throws InterruptedException 
-	 * @throws IOException 
+	 * @throws TasteException 
 	 */
-	public static DataModel[] clusterUsers(DataModel dataModel, int N) throws IOException, InterruptedException, ClassNotFoundException {
-
-		return null;
-	}
-	
-	
-	
-	public static Map<Integer, Integer> invert(Map<Integer, Integer> map) {
-
-	    Map<Integer, Integer> inv = new HashMap<Integer, Integer>();
-
-	    for (Entry<Integer, Integer> entry : map.entrySet())
-	        inv.put(entry.getValue(), entry.getKey());
-
-	    return inv;
-	}
-	
-	public static void writeUsersToFile(List<NamedVector > users, Path path, Configuration conf) throws IOException {
-		FileSystem fs = FileSystem.get(path.toUri(), conf);
-		SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class, VectorWritable.class);
-		VectorWritable vec = new VectorWritable();
+	public static DataModel[] clusterUsers(DataModel dataModel, int N, DistanceMeasure dm) throws IOException, InterruptedException, ClassNotFoundException, TasteException {
 		
-		for (NamedVector user : users) {
-			vec.set(user);
-			// Serializes vector data
-			writer.append(new Text(user.getName()), vec);
-		}
-		writer.close();
+		File testData = new File("clusterdata/users");
+		testData.mkdirs();
 		
-	}
-	
-	public static List<NamedVector> getNamedVector() throws FileNotFoundException {
-		List<NamedVector> users = new ArrayList<NamedVector>();
-		
-		double[][] userItemMatrix = getMatrix("datasets/vtt-36k/VTT_I_data.csv");
-		NamedVector user;
-		
-		Set<Integer> userIds = userPositions.keySet();
-
-		int iterator = 0;
-		for (Integer userId : userIds) {
-			user = new NamedVector(new DenseVector(userItemMatrix[iterator]), ""+userId);
-			users.add(user);
-			iterator++;
-		}
-		
-		return users;
-	}
-	
-	public static double[][] getMatrix(String csvFile) throws FileNotFoundException {
-		itemPositions = new HashMap<Integer, Integer>();
-		userPositions = new HashMap<Integer, Integer>();
-		ratingPairs = new ArrayList<RatingPair>();
-		
-		
-		int itemPosition = 0;
-		int userPosition = 0;
-		
-		File ratings = new File(csvFile);
-		Scanner sc = new Scanner(ratings);
-		sc.useDelimiter("[\n,]");
-		sc.nextLine(); // skip header
-		int counter = 0;
-		
-		RatingPair pair;
-
-		while(sc.hasNextLine() && counter++ <= 5000) {
-			
-			pair = new RatingPair(sc.nextInt(), sc.nextInt());
-			sc.nextInt(); // skip rating
-			
-			ratingPairs.add(pair);
-			
-			if(!userPositions.containsKey(pair.userId)) {
-				userPositions.put(pair.userId, userPosition);
-				userPosition++;
-			}
-			
-			if(!itemPositions.containsKey(pair.itemId)) {
-				itemPositions.put(pair.itemId, itemPosition);
-				itemPosition++;
-			}
-			
-		}
-		sc.close();
-		
-		userItemMatrix = new double[userPosition][itemPosition];
-		
-		for (RatingPair ratingPair : ratingPairs) {
-			userItemMatrix[userPositions.get(ratingPair.userId)][itemPositions.get(ratingPair.itemId)] = 1;
-		}
-
-		return userItemMatrix;
-	}
-
-	public static void main(String[] args) throws TasteException, IOException, InterruptedException, ClassNotFoundException {
-		int numberOfCluster = 10;
-		
-		List<NamedVector> users = getNamedVector();
-		
-		File testData = new File("testdata");
-		if (!testData.exists()) {
-			testData.mkdir();
-		}
-		testData = new File("testdata/points");
-		if (!testData.exists()) {
-			testData.mkdir();
-		}
+		List<NamedVector> users = getUserVectors(dataModel);
 		
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(conf);
-		//Write initial centers
-		writeUsersToFile(users, new Path("testdata/points/file1"), conf);
 		
-		Path path = new Path("testdata/clusters/part-00000");
-		SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class, Kluster.class);
+		// store users in sequence file
+		writeUsers(users, new Path("clusterdata/users/part-00000"), conf);
 		
-		for (int i = 0; i < numberOfCluster; i++) {
-			NamedVector vec = users.get(i);
-			Kluster cluster = new Kluster(vec, i, new EuclideanDistanceMeasure());
-			writer.append(new Text(cluster.getIdentifier()), cluster);
-		}
-		writer.close();
+		// write initial cluster centers
+		writeClusterCenters(users, fs, conf, N, dm);
 		
-		Path output = new Path("output");
+		Path output = new Path("clusterdata/output");
+
+		// clean
 		HadoopUtil.delete(conf, output);
 		
 		// Run k-means algorithm
 		KMeansDriver.run(
-				conf, 							// configuration
-				new Path("testdata/points"), 	// the directory pathname for input points
-				new Path("testdata/clusters"),  // the directory pathname for initial & computed clusters
-				output, 						// the directory pathname for output points
-				new TanimotoDistanceMeasure(), // the DistanceMeasure to use
+				conf, 								// configuration
+				new Path("clusterdata/users"), 	// the directory pathname for input points
+				new Path("clusterdata/clusters"),	// the directory pathname for initial & computed clusters
+				output, 							// the directory pathname for output points
+				dm, 							// the DistanceMeasure to use
 				0.001, 							// the convergence delta value
 				10, 							// the maximum number of iterations
 				true, 							// true if points are to be clustered after iterations are completed
@@ -187,81 +67,94 @@ public class UserClusterer {
 				false							// if true execute sequental algorithm
 			);
 		
-		SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path("output/" 
-				+ Kluster.CLUSTERED_POINTS_DIR + "/part-m-00000"), conf);
+		return readClusters(fs, conf, N);
+	}
+	
+	/**
+	 * Write all users to SequenceFile
+	 * Since kMeans is distributed on Hadoop, it needs the set of users available in this format
+	 */
+	public static void writeUsers(List<NamedVector> users, Path path, Configuration conf) throws IOException {
+		FileSystem fs = FileSystem.get(path.toUri(), conf);
+		Writer writer = new Writer(fs, conf, path, Text.class, VectorWritable.class);		
+		for (NamedVector user : users) {
+			writer.append(new Text(user.getName()), new VectorWritable(user));
+		}
+		writer.close();
+	}
+	
+	/**
+	 * Create N clusters, and use the first N users as cluster centers
+	 */
+	public static void writeClusterCenters(List<NamedVector>users, FileSystem fs, Configuration conf, int N, DistanceMeasure dm) throws IOException {
+		Path path = new Path("clusterdata/clusters/part-00000");
+		Writer writer = new Writer(fs, conf, path, Text.class, Kluster.class);
 		
+		//Write initial cluster centers
+		for (int i = 0; i < N; i++) {
+			Kluster cluster = new Kluster(users.get(i), i, dm);
+			writer.append(new Text(cluster.getIdentifier()), cluster);
+		}
+		writer.close();
+	}
+	
+	public static DataModel[] readClusters(FileSystem fs, Configuration conf, int N) throws IOException {
+		FastByIDMap<PreferenceArray>[] maps = new FastByIDMap[N];
+		for(int i = 0; i < N; i++) {
+			maps[i] = new FastByIDMap<PreferenceArray>();
+		}
+		
+		Path file = new Path("clusterdata/output/" + Kluster.CLUSTERED_POINTS_DIR + "/part-m-00000");
+		// read clusters from Hadoop Sequence File
+		SequenceFile.Reader reader = new SequenceFile.Reader(fs, file, conf);
+		// clusterId store
 		IntWritable clusterId = new IntWritable();
-		WeightedVectorWritable value = new WeightedVectorWritable();
-		Map<String, String> clusters = new HashMap<String, String>(); 
+		// value store
+		VectorWritable value = new VectorWritable();
+		
 		while (reader.next(clusterId, value)) {
 			// Read output, print vector, cluster ID 
-			NamedVector vector = (NamedVector) value.getVector();
-			String vectorName = vector.getName();
-			System.out.println(vectorName + " belongs to cluster " + clusterId.toString());
-			clusters.put(vectorName, clusterId.toString());
+			NamedVector vector = (NamedVector) value.get();
+			long userId = (long) Integer.parseInt(vector.getName());
+			maps[clusterId.get()].put(userId, namedVecToPreferenceArr(vector));
 		}
 		reader.close();
 		
-		writeClustersToFile(clusters, numberOfCluster);
-
-	
-	}
-	
-	public static void writeClustersToFile(Map<String, String> clusters, int numberOfClusters) throws IOException {
-		FileWriter fw = null;
-		PrintWriter pw = null;
-		String fileName;
-		
-		File testData = new File("datasets/vtt-clustered");
-		if (!testData.exists()) {
-			testData.mkdir();
+		DataModel[] models = new DataModel[N];
+		for(int i = 0; i < N; i++) {
+			models[i] = new GenericDataModel(maps[i]);
 		}
-		
-		for (File file : testData.listFiles()) {
-			file.delete();
-		}
-		
-		Map<Integer, Integer> invertedItemPositions = invert(itemPositions);
-		
-		int clusterCounter = 0;
-		
-		// for each cluster
-		for (int i = 0; i < numberOfClusters; i++) {
-			fileName = "datasets/vtt-clustered/cluster" + i + ".csv";
-			fw = new FileWriter(fileName);
-			pw = new PrintWriter(fw);
-			
-			// for each user in cluster i 
-			for (Entry<String, String> entry : clusters.entrySet()) {
-				String clusterId = entry.getValue();
-				String userIdString = entry.getKey();
-				int userId = Integer.parseInt(userIdString);
-				
-				if(i == Integer.parseInt(clusterId)) {
-					clusterCounter++;
-					// for each rating in userItemMatrix[user][i]
-					for (int j = 0; j < userItemMatrix[userPositions.get(userId)].length; j++) {
-						// if rating != 0
-						if(userItemMatrix[userPositions.get(userId)][j] != 0) {
-							pw.print(userId);
-							pw.print(",");
-							pw.print(invertedItemPositions.get(j));
-							pw.print(",");
-							pw.println(1);
-						}
-					}
-				}
-			}
-			
-			System.out.println("Cluster id " + i + " count " + clusterCounter);
-			clusterCounter = 0;
-			
-			pw.flush();
-		}
-		
-		pw.close();
-		fw.close();
+		return models;
 	}
 	
 
+	
+	public static PreferenceArray namedVecToPreferenceArr(NamedVector vec) {
+		PreferenceArray arr = new GenericItemPreferenceArray(vec.size());
+		arr.setUserID(0, Integer.parseInt(vec.getName()));
+		
+		for(int i=0; i < vec.size(); i++) {
+			arr.setValue(i, (float) vec.get(i));
+		}
+		return arr;
+	}
+
+	public static NamedVector preferenceArrToNamedVec(PreferenceArray arr) {
+		DenseVector vec = new DenseVector(arr.length());
+		for(int i=0; i < arr.length(); i++) {
+			vec.set(i, arr.getValue(i));
+		}
+		return new NamedVector(vec, ""+arr.getUserID(0));
+	}
+	
+	public static List<NamedVector> getUserVectors(DataModel dataModel) throws IOException, TasteException {
+		List<NamedVector> users = new ArrayList<NamedVector>();
+		Iterator<Long> userIterator = dataModel.getUserIDs();
+		PreferenceArray prefs;
+		while(userIterator.hasNext()) {
+			prefs = dataModel.getPreferencesFromUser(userIterator.next());
+			users.add(preferenceArrToNamedVec(prefs));
+		}
+		return users;
+	}
 }
