d796c840ef493d4ce2ef6a448d0a03134188e71f
Dag Einar Monsen <me@dag.im>
2013-11-06 13:31:03 +0100

    Rename Clustering to UserClusterer. Rename main() to clusterUsers().

--- src/edu/ntnu/idi/goldfish/UserClusterer.java
+++ src/edu/ntnu/idi/goldfish/UserClusterer.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.clustering.classify.WeightedVectorWritable;
 import org.apache.mahout.clustering.kmeans.KMeansDriver;
 import org.apache.mahout.clustering.kmeans.Kluster;
@@ -31,12 +32,87 @@ import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.NamedVector;
 import org.apache.mahout.math.VectorWritable;
 
-public class Clustering {
+public class UserClusterer {
 	
 	private static Map<Integer, Integer> userPositions;
 	private static Map<Integer, Integer> itemPositions;
 	private static List<RatingPair> ratingPairs;
 	private static Map<Integer, Integer> userToItemMap;
+	
+	/**
+	 * Split the dataModel into a set of datamodels based on a kMeans clustering of the users
+	 * @param dataModel
+	 * @param N
+	 * @return
+	 * @throws ClassNotFoundException 
+	 * @throws InterruptedException 
+	 * @throws IOException 
+	 */
+	public static DataModel[] clusterUsers(DataModel dataModel, int N) throws IOException, InterruptedException, ClassNotFoundException {
+		int numberOfCluster = 3;
+		
+		List<NamedVector> users = getNamedVector();
+		
+		File testData = new File("testdata");
+		if (!testData.exists()) {
+			testData.mkdir();
+		}
+		testData = new File("testdata/points");
+		if (!testData.exists()) {
+			testData.mkdir();
+		}
+		
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		//Write initial centers
+		writeUsersToFile(users, new Path("testdata/points/file1"), conf);
+		
+		Path path = new Path("testdata/clusters/part-00000");
+		SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class, Kluster.class);
+		
+		for (int i = 0; i < numberOfCluster; i++) {
+			NamedVector vec = users.get(i);
+			Kluster cluster = new Kluster(vec, i, new EuclideanDistanceMeasure());
+			writer.append(new Text(cluster.getIdentifier()), cluster);
+		}
+		writer.close();
+		
+		Path output = new Path("output");
+		HadoopUtil.delete(conf, output);
+		
+		// Run k-means algorithm
+		KMeansDriver.run(
+				conf, 							// configuration
+				new Path("testdata/points"), 	// the directory pathname for input points
+				new Path("testdata/clusters"),  // the directory pathname for initial & computed clusters
+				output, 						// the directory pathname for output points
+				new EuclideanDistanceMeasure(), // the DistanceMeasure to use
+				0.001, 							// the convergence delta value
+				20, 							// the maximum number of iterations
+				true, 							// true if points are to be clustered after iterations are completed
+				0.2, 							// clustering strictness/ outlier removal parameter. 
+				false							// if true execute sequental algorithm
+			);
+		
+		SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path("output/" 
+				+ Kluster.CLUSTERED_POINTS_DIR + "/part-m-00000"), conf);
+		
+		IntWritable clusterId = new IntWritable();
+		WeightedVectorWritable value = new WeightedVectorWritable();
+		Map<String, String> clusters = new HashMap<String, String>(); 
+		while (reader.next(clusterId, value)) {
+			// Read output, print vector, cluster ID 
+			NamedVector vector = (NamedVector) value.getVector();
+			String vectorName = vector.getName();
+			System.out.println(vectorName + " belongs to cluster " + clusterId.toString());
+			clusters.put(vectorName, clusterId.toString());
+		}
+		reader.close();
+		
+		writeClustersToFile(clusters, numberOfCluster);
+		return null;
+	}
+	
 	
 	public static void writeUsersToFile(List<NamedVector > users, Path path, Configuration conf) throws IOException {
 		FileSystem fs = FileSystem.get(path.toUri(), conf);
@@ -87,7 +163,7 @@ public class Clustering {
 		int counter = 0;
 		
 		RatingPair pair;
-		while(sc.hasNextLine() && counter++ <= 100) {
+		while(sc.hasNextLine() && counter++ < 10000) {
 			
 			pair = new RatingPair(sc.nextInt(), sc.nextInt());
 			sc.nextInt(); // skip rating
@@ -117,68 +193,8 @@ public class Clustering {
 		return userItemMatrix;
 	}
 
-	public static void main(String[] args) throws TasteException, IOException, InterruptedException, ClassNotFoundException {
-		int numberOfCluster = 3;
-		
-		List<NamedVector> users = getNamedVector();
-		
-		File testData = new File("testdata");
-		if (!testData.exists()) {
-			testData.mkdir();
-		}
-		testData = new File("testdata/points");
-		if (!testData.exists()) {
-			testData.mkdir();
-		}
-		
-		Configuration conf = new Configuration();
-		FileSystem fs = FileSystem.get(conf);
-		//Write initial centers
-		writeUsersToFile(users, new Path("testdata/points/file1"), conf);
-		
-		Path path = new Path("testdata/clusters/part-00000");
-		SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class, Kluster.class);
-		
-		for (int i = 0; i < numberOfCluster; i++) {
-			NamedVector vec = users.get(i);
-			Kluster cluster = new Kluster(vec, i, new EuclideanDistanceMeasure());
-			writer.append(new Text(cluster.getIdentifier()), cluster);
-		}
-		writer.close();
-		
-		Path output = new Path("output");
-		HadoopUtil.delete(conf, output);
-		
-		// Run k-means algorithm
-		KMeansDriver.run(
-				conf, 							// configuration
-				new Path("testdata/points"), 	// the directory pathname for input points
-				new Path("testdata/clusters"),  // the directory pathname for initial & computed clusters
-				output, 						// the directory pathname for output points
-				new EuclideanDistanceMeasure(), // the DistanceMeasure to use
-				0.001, 							// the convergence delta value
-				10, 							// the maximum number of iterations
-				true, 							// true if points are to be clustered after iterations are completed
-				0.0, 							// clustering strictness/ outlier removal parameter. 
-				false							// if true execute sequental algorithm
-			);
-		
-		SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path("output/" 
-				+ Kluster.CLUSTERED_POINTS_DIR + "/part-m-00000"), conf);
-		
-		IntWritable clusterId = new IntWritable();
-		WeightedVectorWritable value = new WeightedVectorWritable();
-		Map<String, String> clusters = new HashMap<String, String>(); 
-		while (reader.next(clusterId, value)) {
-			// Read output, print vector, cluster ID 
-			NamedVector vector = (NamedVector) value.getVector();
-			String vectorName = vector.getName();
-			System.out.println(vectorName + " belongs to cluster " + clusterId.toString());
-			clusters.put(vectorName, clusterId.toString());
-		}
-		reader.close();
-		
-		writeClustersToFile(clusters, numberOfCluster);
+	public static void make(String[] args) throws TasteException, IOException, InterruptedException, ClassNotFoundException {
+
 	
 	}
 	
