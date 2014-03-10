34f75dda84b5c52ac116bf67084ba6e1d6474f1e
Dag Einar Monsen <me@dag.im>
2014-03-10 13:23:13 +0100

    Add overriding method on SMRMSEevaluator: splitOneUsersPrefs()

--- src/edu/ntnu/idi/goldfish/mahout/SMRMSEevaluator.java
+++ src/edu/ntnu/idi/goldfish/mahout/SMRMSEevaluator.java
@@ -1,9 +1,19 @@
 package edu.ntnu.idi.goldfish.mahout;
 
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.FastByIDMap;
 import org.apache.mahout.cf.taste.impl.common.FullRunningAverage;
 import org.apache.mahout.cf.taste.impl.common.RunningAverage;
 import org.apache.mahout.cf.taste.impl.eval.AbstractDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.GenericPreference;
+import org.apache.mahout.cf.taste.impl.model.GenericUserPreferenceArray;
+import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.model.Preference;
+import org.apache.mahout.cf.taste.model.PreferenceArray;
+
+import com.google.common.collect.Lists;
 
 public class SMRMSEevaluator extends AbstractDifferenceRecommenderEvaluator {
 
@@ -28,6 +38,38 @@ public class SMRMSEevaluator extends AbstractDifferenceRecommenderEvaluator {
 	@Override
 	public String toString() {
 		return "RMSRecommenderEvaluator";
+	}
+
+	protected void splitOneUsersPrefs(double trainingPercentage, FastByIDMap<PreferenceArray> trainingPrefs,
+			FastByIDMap<PreferenceArray> testPrefs, long userID, DataModel dataModel) throws TasteException {
+		List<Preference> oneUserTrainingPrefs = null;
+		List<Preference> oneUserTestPrefs = null;
+		PreferenceArray prefs = dataModel.getPreferencesFromUser(userID);
+		int size = prefs.length();
+		for (int i = 0; i < size; i++) {
+			Preference newPref = new GenericPreference(userID, prefs.getItemID(i), prefs.getValue(i));
+			
+			//@TODO keep pseudoratings in training set while keeping correct trainingPercentage
+			
+			if (i < trainingPercentage) {
+				if (oneUserTrainingPrefs == null) {
+					oneUserTrainingPrefs = Lists.newArrayListWithCapacity(3);
+				}
+				oneUserTrainingPrefs.add(newPref);
+			} 
+			else {
+				if (oneUserTestPrefs == null) {
+					oneUserTestPrefs = Lists.newArrayListWithCapacity(3);
+				}
+				oneUserTestPrefs.add(newPref);
+			}
+		}
+		if (oneUserTrainingPrefs != null) {
+			trainingPrefs.put(userID, new GenericUserPreferenceArray(oneUserTrainingPrefs));
+			if (oneUserTestPrefs != null) {
+				testPrefs.put(userID, new GenericUserPreferenceArray(oneUserTestPrefs));
+			}
+		}
 	}
 
 }
