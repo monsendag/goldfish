58d48f45f36a9a2f13861d011c1091d8a241ec96
Patrick Heia Romstad <promstad1@gmail.com>
2014-03-12 12:09:20 +0100

    Added regression as a way to create pseudoratings

---
+++ src/edu/ntnu/idi/goldfish/TrendLine.java
@@ -0,0 +1,6 @@
+package edu.ntnu.idi.goldfish;
+
+public interface TrendLine {
+	public void setValues(double[] x, double[] y); // y ~ f(x)
+	public double predict(double x); // get a predicted y for a given x
+}
