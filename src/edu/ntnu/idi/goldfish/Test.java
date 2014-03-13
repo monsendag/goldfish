53660e1735671c27286ce1f5cd17d1a26279c538
Dag Einar Monsen <me@dag.im>
2014-03-13 13:30:44 +0100

    Clear Test body

--- src/edu/ntnu/idi/goldfish/Test.java
+++ src/edu/ntnu/idi/goldfish/Test.java
@@ -1,19 +1,10 @@
 package edu.ntnu.idi.goldfish;
 
-import edu.ntnu.idi.goldfish.mahout.SMDataModel;
-import org.apache.mahout.cf.taste.common.TasteException;
-
-import java.io.File;
-import java.io.IOException;
-
 public class Test {
 
-	public static void main(String[] args) throws IOException, TasteException {
-		
-		SMDataModel model = new SMDataModel(new File("datasets/movielens-synthesized/ratings.csv"));
-		model.writeDatasetToFile("/tmp/ratings-synthesized.csv");
-		System.out.println(String.format("users: %d, items: %d", model.getNumUsers(), model.getNumItems()));
-		
+	public static void main(String[] args) throws Exception {
+
+
 	}
 	
 }
