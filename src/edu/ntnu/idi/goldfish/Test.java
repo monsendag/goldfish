c62e7b689702f1e24472d735da90684973d972f8
Dag Einar Monsen <me@dag.im>
2014-03-13 14:14:55 +0100

    Add testing code of Preprocessor to Test class

--- src/edu/ntnu/idi/goldfish/Test.java
+++ src/edu/ntnu/idi/goldfish/Test.java
@@ -1,9 +1,20 @@
 package edu.ntnu.idi.goldfish;
 
+import edu.ntnu.idi.goldfish.mahout.SMDataModel;
+import edu.ntnu.idi.goldfish.preprocessors.Preprocessor;
+
+import java.io.File;
+
 public class Test {
 
 	public static void main(String[] args) throws Exception {
+        SMDataModel model;
+        model = new SMDataModel(new File("datasets/yow-userstudy/like-timeonpage-timeonmouse.csv"));
 
+        System.out.println(String.format("Density unprocessed: %f", model.getDensity()));
+        Preprocessor pre = new Preprocessor();
+        pre.preprocess(model);
+        System.out.println(String.format("Density processed: %f", model.getDensity()));
 
 	}
 	
