a4e08b844df083804454f4b098010ab91f968b91
Dag Einar Monsen <me@dag.im>
2014-03-31 17:15:40 +0200

    Many changes to YowModel
    - Rename DB to table
    - Add a few more support methods
    - Move preprocessing code to class implementing Preprocessor
    - Remove debugging code from main() method

--- src/edu/ntnu/idi/goldfish/preprocessors/YowModel.java
+++ src/edu/ntnu/idi/goldfish/preprocessors/YowModel.java
@@ -11,20 +11,18 @@ import org.apache.mahout.cf.taste.impl.model.GenericDataModel;
 import org.apache.mahout.cf.taste.impl.model.GenericItemPreferenceArray;
 import org.apache.mahout.cf.taste.impl.model.GenericPreference;
 import org.apache.mahout.cf.taste.impl.model.GenericUserPreferenceArray;
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.model.Preference;
 import org.apache.mahout.cf.taste.model.PreferenceArray;
+import org.h2.tools.Server;
 import org.jooq.*;
 import org.jooq.impl.DSL;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.*;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.*;
 
 import static org.jooq.impl.DSL.fieldByName;
 import static org.jooq.impl.DSL.tableByName;
@@ -40,75 +38,39 @@ public class YowModel implements DataModel {
      */
     private static final long serialVersionUID = 1L;
     DSLContext context;
-    private Table<Record> DB = tableByName("yow");
+    private Table<Record> table = tableByName("yow");
     private Field<Long> userField = fieldByName(Long.class, "userid");
     private Field<Long> itemField = fieldByName(Long.class, "itemid");
     private Field<Long> fbackField = fieldByName(Long.class, "feedback");
     private Field<Float> valueField = fieldByName(Float.class, "value");
 
     public static void main(String[] args) throws Exception {
-        File file = new File("datasets/yow-userstudy/ratings-fixed.csv");
-        DataModel fm = new FileDataModel(file);
-        DataModel ym = new YowModel(file);
 
-        LongPrimitiveIterator users = fm.getUserIDs();
-        while(users.hasNext()) {
-            long userid = users.nextLong();
-            PreferenceArray fprefs = fm.getPreferencesFromUser(userid);
-            PreferenceArray yprefs = ym.getPreferencesFromUser(userid);
-
-            fprefs.sortByItem();
-            yprefs.sortByItem();
-
-            Iterator<Preference> i1 = fprefs.iterator();
-            Iterator<Preference> i2 = yprefs.iterator();
-
-            while(i1.hasNext()) {
-                Preference p1 = i1.next();
-                Preference p2 = i2.next();
-
-                if(p1.getValue() != p2.getValue() || p1.getUserID() != p2.getUserID() || p1.getItemID() != p2.getItemID()) {
-                    System.out.println("koko");
-                }
-            }
-        }
+        YowModel model = new YowModel(new File("datasets/yow-userstudy/exdupes-like-timeonpage-timeonmouse.csv"));
+        Server webServer = Server.createWebServer("-webAllowOthers").start();
+        System.out.println("jdbc:h2:mem:"+model.hashCode());
+        System.out.printf("users: %d, items: %d", model.getNumUsers(), model.getNumItems());
     }
 
     public YowModel(File f) throws Exception {
-        // load driver
-        Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:mem:yow;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false");
-        context = DSL.using(conn, SQLDialect.MYSQL);
-
-        //Server webServer = Server.createWebServer("-webAllowOthers").start();
-
-        initTable();
+        initDB();
         parseFile(f);
+    }
 
-        System.out.println(getNumUsers());
+    public YowModel(FastByIDMap<PreferenceArray> userData) {
+        initDB();
+        Field[] fields = new Field[]{userField, itemField, fbackField, valueField};
+        Object[] values;
 
-        LongPrimitiveIterator users = getUserIDs();
-
-        while(users.hasNext()) {
-            long userID = users.next();
-
-            LongPrimitiveIterator fbackIDs = getFeedbackIDs();
-
-            FastByIDMap<PreferenceArray> feedbackData = new FastByIDMap<>();
-
-            while(fbackIDs.hasNext()) {
-                long fbackID = fbackIDs.next();
-                feedbackData.put(fbackID, getPreferencesFromUserFeedback(userID, fbackID));
+        LongPrimitiveIterator userIter = userData.keySetIterator();
+        while(userIter.hasNext()) {
+            long userID = userIter.next();
+            PreferenceArray prefs = userData.get(userID);
+            for(Preference p : prefs) {
+                values = new Object[]{p.getUserID(), p.getItemID(), EXPLICIT, p.getValue()};
+                context.insertInto(table, fields).values(values).execute();
             }
-            // create pref X item matrix
-            DataModel model = new GenericDataModel(feedbackData);
-
-            // do recommendation on model -- Fill all data for "user" EXPLICIT
-            // model preference name as user id
         }
-
-        // merge all datamodels to final model -> return it
-
     }
 
     private void parseFile(File f) throws FileNotFoundException {
@@ -136,29 +98,38 @@ public class YowModel implements DataModel {
         Field[] fields = new Field[]{userField, itemField, fbackField, valueField};
 
         if (explicit > 0) {
-            context.insertInto(DB, fields).values(new Object[]{userID, itemID, EXPLICIT, explicit}).execute();
+            context.insertInto(table, fields).values(new Object[]{userID, itemID, EXPLICIT, explicit}).execute();
         }
         if(timeonpage > 0) {
-            context.insertInto(DB, fields).values(new Object[]{userID, itemID, TIMEONPAGE, timeonpage}).execute();
+            context.insertInto(table, fields).values(new Object[]{userID, itemID, TIMEONPAGE, timeonpage}).execute();
         }
         if(timeonmouse > 0) {
-            context.insertInto(DB, fields).values(new Object[]{userID, itemID, TIMEONMOUSE, timeonmouse}).execute();
+            context.insertInto(table, fields).values(new Object[]{userID, itemID, TIMEONMOUSE, timeonmouse}).execute();
         }
     }
 
-    private void initTable() {
-        // initialize db
-        context.query("CREATE TABLE IF NOT EXISTS yow (" +
-                "userid INT NOT NULL, " +
-                "itemid INT NOT NULL, " +
-                "feedback INT NOT NULL, " +
-                "value INT NOT NULL " +
-                "" +
-                ")").execute();
+    private void initDB() {
+        try {
+            // load driver
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:mem:"+hashCode());
+            context = DSL.using(conn, SQLDialect.MYSQL);
 
-        // set indices
-        context.query("CREATE INDEX uindex ON yow (userid)").execute();
-        context.query("CREATE INDEX iindex ON yow (itemid)").execute();
+            // initialize db
+            context.query("CREATE TABLE IF NOT EXISTS yow (" +
+                    "userid INT NOT NULL, " +
+                    "itemid INT NOT NULL, " +
+                    "feedback INT NOT NULL, " +
+                    "value INT NOT NULL " +
+                    "" +
+                    ")").execute();
+
+            // set indices
+            context.query("CREATE INDEX uindex ON yow (userid)").execute();
+            context.query("CREATE INDEX iindex ON yow (itemid)").execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public FastByIDMap<PreferenceArray> getUserData() throws TasteException {
@@ -173,32 +144,32 @@ public class YowModel implements DataModel {
 
     public LongPrimitiveIterator getFeedbackIDs() throws TasteException {
         Long[] fbackIDs = context.selectDistinct(fbackField)
-                .from(DB).fetchArray(fbackField);
+                .from(table).fetchArray(fbackField);
         return new LongPrimitiveArrayIterator(ArrayUtils.toPrimitive(fbackIDs));
     }
 
     public LongPrimitiveIterator getUserIDs() throws TasteException {
         Long[] userIDs = context.selectDistinct(userField)
-                .from(DB).fetchArray(userField);
+                .from(table).fetchArray(userField);
         return new LongPrimitiveArrayIterator(ArrayUtils.toPrimitive(userIDs));
     }
 
     public LongPrimitiveIterator getItemIDs() throws TasteException {
         Long[] itemID = context.selectDistinct(itemField)
-                .from(DB).fetchArray(itemField);
+                .from(table).fetchArray(itemField);
         return new LongPrimitiveArrayIterator(ArrayUtils.toPrimitive(itemID));
     }
 
     public PreferenceArray getPreferencesFromUserFeedback(long userID, long fbackID) throws TasteException {
-        List<? extends Preference> prefs = context.select(fbackField, itemField, valueField).from(DB)
-                .where(userField.equal(fbackID))
+        List<? extends Preference> prefs = context.select(fbackField, itemField, valueField).from(table)
+                .where(userField.equal(userID))
                 .and(fbackField.equal(fbackID))
                 .fetch().into(GenericPreference.class);
         return new GenericUserPreferenceArray(prefs);
     }
 
     public PreferenceArray getPreferencesFromUser(long userID) throws TasteException {
-        List<? extends Preference> prefs = context.select(userField, itemField, valueField).from(DB)
+        List<? extends Preference> prefs = context.select(userField, itemField, valueField).from(table)
                 .where(userField.equal(userID))
                 .and(fbackField.equal(EXPLICIT))
                 .fetch().into(GenericPreference.class);
@@ -206,7 +177,7 @@ public class YowModel implements DataModel {
     }
 
     public PreferenceArray getPreferencesForItem(long itemID) throws TasteException {
-        List<? extends Preference> prefs = context.select().from(DB)
+        List<? extends Preference> prefs = context.select(userField, itemField, valueField).from(table)
                 .where(itemField.equal(itemID))
                 .and(fbackField.equal(EXPLICIT))
                 .fetch().into(GenericPreference.class);
@@ -215,19 +186,20 @@ public class YowModel implements DataModel {
 
     public FastIDSet getItemIDsFromUser(long userID) throws TasteException {
         // fetch all itemIDs for specific user, return a fastIDSet!
-        List<Long> itemIDs = context.selectDistinct(itemField).from(DB).where(userField.equal(userID)).fetch(itemField);
+        List<Long> itemIDs = context.selectDistinct(itemField).from(table).where(userField.equal(userID)).fetch(itemField);
         FastIDSet set = new FastIDSet();
         itemIDs.stream().forEach(set::add);
         return set;
     }
 
 
+
     @Override
     public Float getPreferenceValue(long userID, long itemID) throws TasteException {
-        return context.selectOne().from(DB)
+        return context.select(valueField).from(table)
                 .where(userField.equal(userID)
-                        .and(itemField.equal(itemID)))
-                .and(fbackField.equal(EXPLICIT))
+                        .and(itemField.equal(itemID))
+                        .and(fbackField.equal(EXPLICIT)))
                 .fetchOne(valueField);
     }
 
@@ -238,36 +210,36 @@ public class YowModel implements DataModel {
 
     @Override
     public int getNumItems() throws TasteException {
-        return context.selectCount().from(DB).groupBy(itemField).fetchCount();
+        return context.selectCount().from(table).groupBy(itemField).fetchCount();
     }
 
     @Override
     public int getNumUsers() throws TasteException {
-        return context.selectCount().from(DB).groupBy(userField).fetchCount();
+        return context.selectCount().from(table).groupBy(userField).fetchCount();
     }
 
     @Override
     public int getNumUsersWithPreferenceFor(long itemID) throws TasteException {
-        return context.selectDistinct(userField).from(DB)
+        return context.selectDistinct(userField).from(table)
                 .where(itemField.equal(itemID))
                 .groupBy(userField).fetchCount();
     }
 
     @Override
     public int getNumUsersWithPreferenceFor(long itemID1, long itemID2) throws TasteException {
-        return context.selectDistinct(userField).from(DB)
+        return context.selectDistinct(userField).from(table)
                 .where(itemField.equal(itemID1)
                         .or(itemField.equal(itemID2))).fetchCount();
     }
 
     @Override
     public void setPreference(long userID, long itemID, float value) throws TasteException {
-
+        context.mergeInto(table, userField, itemField, fbackField, valueField).key(userField, itemField, fbackField).values(userID, itemID, EXPLICIT, value);
     }
 
     @Override
     public void removePreference(long userID, long itemID) throws TasteException {
-        context.delete(DB).where(userField.equal(userID).and(itemField.equal(itemID)));
+        context.delete(table).where(userField.equal(userID).and(itemField.equal(itemID)));
     }
 
     @Override
@@ -277,16 +249,28 @@ public class YowModel implements DataModel {
 
     @Override
     public float getMaxPreference() {
-        return context.select(DSL.max(valueField)).fetchOne(valueField);
+        return context.select(DSL.max(valueField)).from(table).fetchOne(DSL.max(valueField));
     }
 
     @Override
     public float getMinPreference() {
-        return context.select(DSL.min(valueField)).fetchOne(valueField);
+        return context.select(DSL.min(valueField)).from(table).fetchOne(DSL.min(valueField));
     }
 
     @Override
     public void refresh(Collection<Refreshable> alreadyRefreshed) {
         // reload file? Reload from mongo?
+    }
+
+    public DataModel toMemory() throws TasteException {
+        return new GenericDataModel(getUserData());
+    }
+
+    public Map<Long, Float> getFeedback() {
+        return context.select(valueField).from(table).groupBy(userField, itemField, fbackField).fetchMap(fbackField, valueField);
+    }
+
+    public Result<Record5<Long, Long, Float, Float, Float>> getFeedbackRows() {
+        return context.select(userField, itemField, valueField, valueField, valueField).groupBy(userField, itemField).fetch();
     }
 }
