6bf0584e870c1a1e4f493a8799d34e695a64de50
Patrick Heia Romstad <promstad1@gmail.com>
2014-04-03 15:04:55 +0200

    Preprocessing with clusters do now work

--- src/edu/ntnu/idi/goldfish/preprocessors/PreprocessorClustering.java
+++ src/edu/ntnu/idi/goldfish/preprocessors/PreprocessorClustering.java
@@ -2,7 +2,11 @@ package edu.ntnu.idi.goldfish.preprocessors;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
@@ -12,13 +16,19 @@ import org.apache.mahout.cf.taste.model.PreferenceArray;
 
 import edu.ntnu.idi.goldfish.mahout.SMDataModel;
 import edu.ntnu.idi.goldfish.mahout.SMPreference;
+import weka.classifiers.meta.ClassificationViaClustering;
+import weka.clusterers.ClusterEvaluation;
+import weka.clusterers.FilteredClusterer;
 import weka.clusterers.SimpleKMeans;
 import weka.core.Instances;
 import weka.core.Instance;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
 public class PreprocessorClustering extends Preprocessor{
 
-	private SimpleKMeans clusterer = null;
+	private ClassificationViaClustering cvc = null;
+	private Instances data = null;
 	
 	public static DataModel getPreprocessedDataModel(String path) throws Exception {
 		SMDataModel model;
@@ -27,31 +37,34 @@ public class PreprocessorClustering extends Preprocessor{
 		pre.preprocess(model);
 		return model;
 	}
+
+	public static void main(String[] args) throws Exception {
+		PreprocessorClustering pc = new PreprocessorClustering();
+	}
 	
 	public PreprocessorClustering() throws Exception{
-		String[] options = new String[1];
-		options[0] = "-V";
-		clusterer = new SimpleKMeans();
 		
+		// create the simpleKMeans cluster
+		SimpleKMeans skm = new SimpleKMeans();
+		skm.setNumClusters(5);
+				
+		//read the dataset and create instances for training and evaluating
 		BufferedReader reader = new BufferedReader(
-				new FileReader("datasets/yow-userstudy/yow-preprocess-clustering.arff"));
-		Instances data = new Instances(reader);
+				new FileReader("datasets/yow-userstudy/arff/yow-preprocess-clustering.arff"));
+		data = new Instances(reader);
+		data.setClassIndex(0);
 		
-		clusterer.setOptions(options);
-		clusterer.setNumClusters(5);
-		clusterer.buildClusterer(data);
+		cvc = new ClassificationViaClustering();
+		cvc.setClusterer(skm);
+		cvc.buildClassifier(data);
 		
-		System.out.println(clusterer.toString());
-		
+		System.out.println(cvc.toString());
 		
 	}
-	
-	public int getRating(double timeOnPage, double timeOnMouse) throws Exception{
-		return clusterer.clusterInstance(new Instance(1, new double[]{timeOnPage,timeOnMouse}));
-	}
-	
+		
 	public void preprocess(SMDataModel model) throws Exception {
 		
+		int counter = 0;
 		// iterate through all items
 		LongPrimitiveIterator it = model.getItemIDs();
 		while (it.hasNext()) {
@@ -63,18 +76,18 @@ public class PreprocessorClustering extends Preprocessor{
 			for (Preference p : prefs) {
 	
 				SMPreference pref = (SMPreference) p;
-				boolean hasExplicit = pref.getValue(0) >= 1;
+				boolean hasExplicit = pref.getValue(RATING_INDEX) >= 1;
 	
 				if (!hasExplicit) {
-					float[] feedback = pref.getValues();
-					int centroid = clusterer.clusterInstance(new Instance(1, 
-							new double[]{feedback[1],feedback[2]}));
+					Instance i = new Instance(1, new double[]{-1, pref.getValue(TIME_ON_PAGE_INDEX), 
+							pref.getValue(TIME_ON_MOUSE_INDEX)});
+					i.setDataset(data);
+					int pseudorating = (int) (cvc.classifyInstance(i) + 1);
 					
-					System.out.println(String.format("User %d gave item %d a pseudorating of: %d", 
-							pref.getUserID(), pref.getItemID(), centroid));
-					int pseudorating = centroid;
+					System.out.println(String.format("%d: User %d gave item %d a pseudorating of: %d", 
+							++counter, pref.getUserID(), pref.getItemID(), pseudorating));
 					
-					pref.setValue(pseudorating, 0);
+					pref.setValue(pseudorating, RATING_INDEX);
 					pseudoRatings.add(String.format("%d_%d", pref.getUserID(), pref.getItemID()));
 				}
 			}
@@ -85,5 +98,58 @@ public class PreprocessorClustering extends Preprocessor{
 	public DataModel preprocess(YowModel model) throws TasteException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void ManualClassificationViaClustering() throws Exception{
+		
+		// create the simpleKMeans cluster
+		SimpleKMeans clusterer = new SimpleKMeans();
+		String[] options = new String[1];
+		options[0] = "-V";
+		clusterer.setOptions(options);
+		clusterer.setNumClusters(5);
+		
+		//read the dataset and create instances for training and evaluating
+		BufferedReader reader = new BufferedReader(
+				new FileReader("datasets/yow-userstudy/arff/yow-preprocess-clustering.arff"));
+		Instances data = new Instances(reader);
+		Instances evalData = new Instances(data);
+		evalData.setClassIndex(0);
+
+		// remove the class index (user_like) since we are going to create clusters with time on page and mouse
+		data = removeAttribute(data,1);
+		
+		clusterer.buildClusterer(data);
+		
+		// evaluate clusterer
+	    ClusterEvaluation eval = new ClusterEvaluation();
+	    eval.setClusterer(clusterer);
+	    eval.evaluateClusterer(evalData);
+	    
+		System.out.println(eval.clusterResultsToString());
+
+		Map<Integer,Integer> clusterToRating = new HashMap<Integer,Integer>();
+		// map clusters to ratings
+		int[] classToCluster = eval.getClassesToClusters();
+		for (int i = 0; i < classToCluster.length; i++) {
+			clusterToRating.put(i, (classToCluster[i]+1));
+		}
+		
+//		<--- Put this where the pseduorating is going to be calculated --->
+//		int cluster = clusterer.clusterInstance(new Instance(1, 
+//		new double[]{pref.getValue(TIME_ON_PAGE_INDEX),pref.getValue(TIME_ON_MOUSE_INDEX)}));
+//
+//		int pseudorating = clusterToRating.get(cluster);
+	}
+	
+	private Instances removeAttribute(Instances data, int attributeIndex) throws Exception {
+		Remove remove = new Remove(); // new instance of filter
+		String[] removeOptions = new String[2];
+		removeOptions[0] = "-R"; // "range"
+		removeOptions[1] = ""+attributeIndex; // we want to ignore the attribute that is in the position '1' (first)
+		remove.setOptions(removeOptions); // set options
+		remove.setInputFormat(data); // inform filter about dataset
+		data = Filter.useFilter(data, remove);
+		return data;
 	}
 }
