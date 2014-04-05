bae7ad4936df34ee3e3b2e952c30da6cd32255b2
Patrick Heia Romstad <promstad1@gmail.com>
2014-04-05 16:55:02 +0200

    Multiple linear regression works with DBModel
    However, should be modified to be able to handle different number of independent variables

--- src/edu/ntnu/idi/goldfish/preprocessors/PreprocessorMLR.java
+++ src/edu/ntnu/idi/goldfish/preprocessors/PreprocessorMLR.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.goldfish.preprocessors;
 
 import edu.ntnu.idi.goldfish.mahout.DBModel;
+import edu.ntnu.idi.goldfish.mahout.DBModel.DBRow;
 import edu.ntnu.idi.goldfish.mahout.SMPreference;
 
 import org.apache.commons.math3.exception.NumberIsTooSmallException;
@@ -11,11 +12,14 @@ import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.model.Preference;
 import org.apache.mahout.cf.taste.model.PreferenceArray;
 
+import weka.core.Instance;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * This class is used to map implicit feedback to explicit rating where it is possible.
@@ -24,111 +28,74 @@ import java.util.Map;
  */
 public class PreprocessorMLR extends Preprocessor {
 
-	private final int THRESHOLD = 3;
 	private final int TIME_ON_PAGE_INDEX = 1;
 	private final int TIME_ON_MOUSE_INDEX = 2;
 	private final int RATING_INDEX = 0;
 	
 	private Map<String, Float> correlations = new HashMap<String, Float>();
 
+	public static void main(String[] args) throws Exception {
+		DataModel model = PreprocessorMLR.getPreprocessedDataModel("datasets/yow-userstudy/python/yow-smart-sample-implicit-2.csv");
+	}
 
 	public static DataModel getPreprocessedDataModel(String path) throws Exception {
 		DBModel model;
 		model = new DBModel(new File(path));
 		PreprocessorMLR pre = new PreprocessorMLR();
-		pre.preprocess(model);
+		pre.preprocess(model, 2);
 		return model;
 	}
 
 
-	public DataModel preprocess(DBModel model) {
-		try {
-			
-			double[] beta = globalLR(model, 2);
-			
-			LongPrimitiveIterator it = model.getItemIDs();
-			while(it.hasNext()){
-				long itemID = it.next();
-				PreferenceArray prefs = model.getPreferencesForItem(itemID);
-				
-				for (Preference p : prefs) {
-					
-					SMPreference pref = (SMPreference) p;
-					boolean hasExplicit = pref.getValue(RATING_INDEX) >= 1;
-					if(hasExplicit){
-						// No need to add a pseudo rating
-						continue;
-					}
-					
-					float[] feedback = pref.getValues();
-					
-					float pseudoRating = (float) beta[0];
-					for (int i = 1; i < beta.length; i++) {
-						pseudoRating += beta[i]*feedback[i];
-					}
-					pref.setValue(Math.round(pseudoRating), RATING_INDEX);
-					pseudoRatings.add(String.format("%d_%d", pref.getUserID(), pref.getItemID()));
-				}
-			}
-		} catch (TasteException e) {
-			e.printStackTrace();
-			System.err.println("Could not get all items from the yow model");
-		}
-        return null;
+	public DataModel preprocess(DBModel model, int numberOfIndependentVariables) throws TasteException {
 		
+		double[] beta = globalLR(model, numberOfIndependentVariables);
+		
+		List<DBModel.DBRow> results = model.getFeedbackRows().stream().filter(row -> row.rating == 0).collect(Collectors.toList());
+		for(DBModel.DBRow row : results) {
+			
+			float pseudoRating = (float) beta[RATING_INDEX];
+			pseudoRating += beta[TIME_ON_PAGE_INDEX] * row.timeonpage;
+			pseudoRating += beta[TIME_ON_MOUSE_INDEX] * row.timeonmouse;
+			
+			// the beta0 is 3, have to manually set the lowest ratings
+			pseudoRating = row.timeonpage < 25000 ? 2 : pseudoRating;
+			pseudoRating = row.timeonpage < 10000 ? 1 : pseudoRating;
+			
+			// is pseudorating > 5, then outlier feedback has been used
+			pseudoRating = pseudoRating > 5 ? -1 : pseudoRating;
+			
+			System.out.format("linear regression: u: %d  i: %6d  estimate: %d\n", row.userid, row.itemid, Math.round(pseudoRating));
+			
+			model.setPreference(row.userid, row.itemid, (float) Math.round(pseudoRating));
+			pseudoRatings.add(String.format("%d_%d", row.userid, row.itemid));
+		}
+		
+		return model;
 	}
 	
 	private double[] globalLR(DBModel model, int numberOfIndependentVariables) throws TasteException {
 		
 		if(numberOfIndependentVariables == 0) throw new NumberIsTooSmallException(numberOfIndependentVariables, 1, true);
 		
-		List<Double> tempExpl = new ArrayList<Double>();
-		List<Double[]> tempImpl = new ArrayList<Double[]>();
-		int implSize = numberOfIndependentVariables;
-		Double[] impl = new Double[implSize];
+		List<DBModel.DBRow> results = model.getFeedbackRows().stream().filter(row -> row.rating > 0).collect(Collectors.toList());
+		double[] dependentVariables = new double[results.size()]; // the explicit ratings to infer
+		double[][] independentVariables = new double[results.size()][]; // the implicit feedback
+		double[] implicitFeedback = null;
+		int index = 0;
 		
-		LongPrimitiveIterator it = model.getItemIDs();
-		while (it.hasNext()) {
+		for (DBRow row : results) {
+			dependentVariables[index] = row.rating;
 			
-			long itemID = it.next();
-			PreferenceArray prefs = model.getPreferencesForItem(itemID);
-			SMPreference checkIVs = (SMPreference) prefs.get(0);
-			if(checkIVs.getValues().length-1 < implSize){
-				implSize = checkIVs.getValues().length-1;
-			}
-			
-			for (int i = 0; i < prefs.length(); i++) {
-				SMPreference p = (SMPreference) prefs.get(i);
-				// ensure that we have explicit value
-				if(p.getValue(RATING_INDEX) <= 0 || p.getValue(TIME_ON_PAGE_INDEX) <= 0) {
-					continue;
-				}
-				
-				tempExpl.add((double) p.getValue(RATING_INDEX));
-				impl = new Double[implSize];
-				for (int j = 0; j < impl.length; j++) {
-					impl[j] = (double) p.getValue(j+1);
-				}
-				tempImpl.add(impl);
-			}
-		}
-		
-		if(tempExpl.size() != tempImpl.size()) throw new NumberFormatException("LR must have equal IV and DV sizes");
-	
-		double[] explRatings = new double[tempExpl.size()];
-		double[][] implRatings = new double[tempImpl.size()][];
-		for (int i = 0; i < explRatings.length; i++) {
-			explRatings[i] = tempExpl.get(i);
-			Double[] tempRatings = tempImpl.get(i);
-			double[] ratings = new double[tempRatings.length];
-			for (int j = 0; j < tempRatings.length; j++) {
-				ratings[j] = tempRatings[j];
-			}
-			implRatings[i] = ratings;
+			implicitFeedback = new double[2];
+			implicitFeedback[0] = row.timeonpage;
+			implicitFeedback[1] = row.timeonmouse;
+			independentVariables[index] = implicitFeedback;
+			index++;
 		}
 		
 		OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
-		regression.newSampleData(explRatings, implRatings);
+		regression.newSampleData(dependentVariables, independentVariables);
 
 		double[] beta = regression.estimateRegressionParameters();      
 		
@@ -139,6 +106,13 @@ public class PreprocessorMLR extends Preprocessor {
 		System.out.println("");
 		
 		return beta;
+	}
+
+
+	@Override
+	protected DataModel preprocess(DBModel model) throws Exception {
+		// time on page is default
+		return preprocess(model, 1); 
 	}
 
 	
